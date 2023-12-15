6b02813790341a22b2e979ead0c4e90da974ea0b
AmalieSenniksen <126453118+AmalieSenniksen@users.noreply.github.com>
2023-12-15 22:56:23 +0100

    Dagens arbejde

--- script_undersider.js
+++ script_undersider.js
@@ -1,3 +1,4 @@
+/* Pølserækken vises langsomt */
 
 const krop = document.querySelector("body");
 const firkant = document.getElementById("kasse");
@@ -10,3 +11,26 @@ function flytFirkant() {
 }
 
 krop.onscroll = flytFirkant;
+
+//BURGERMENU 
+
+/* Lukke  */
+
+/* Åbne */
+function visMenu() {
+    document.getElementById("nav_menu").style.display = "block";
+    document.getElementById("burgermenu").style.display = "none";
+    document.getElementById("burgerkryds").style.display = "block";
+    document.getElementById("nav_menu").style.backgroundColor = "#FFFDF6";
+  }
+  
+  document.getElementById("burgermenu").onclick= visMenu;
+  
+  function gemMenu() {
+    document.getElementById("nav_menu").style.display = "none";
+    document.getElementById("burgerkryds").style.display = "none";
+    document.getElementById("burgermenu").style.display = "block";
+  }
+  
+  document.getElementById("burgerkryds").onclick= gemMenu;
+  
