d76b0300ea90bf81ca7750292e4cb020a1a8a920
AmalieSenniksen <126453118+AmalieSenniksen@users.noreply.github.com>
2023-12-17 17:52:36 +0100

    Allersidste ændring

--- script.js
+++ script.js
@@ -15,19 +15,6 @@ setTimeout(function() {
   pladsen.style.display = "block";
 }, 3000); 
 
-/* Pølserækken vises langsomt */
-
-const krop = document.querySelector("body");
-const firkant = document.getElementById("kasse");
-let scrollPosition;
-
-function flytFirkant() {
-    scrollPosition = Math.round(window.scrollY);
-    scrollPosition = scrollPosition/8;
-    firkant.style.top = scrollPosition + "px";
-}
-
-krop.onscroll = flytFirkant;
 
 //BURGERMENU 
 
